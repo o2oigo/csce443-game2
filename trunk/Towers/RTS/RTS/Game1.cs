0cb20c4e5208c397c5fd8a9eb005c743fb0ab77f
afleshner <afleshner@282f65b9-ccb7-5106-6a69-96a0916cf8f3>
2011-10-07 15:17:40 +0000

    Initial Commit with Tank Code

---
+++ trunk/Towers/RTS/RTS/Game1.cs
@@ -0,0 +1,212 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace RTS
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player;
+        //Enemy enemy;
+        List<Enemy> enemies;
+        float enemyTimer = 0;
+        float destroyed = 0;
+        Random rand = new Random();
+        public ExplosionParticleSystem explosion;
+        public ExplosionParticleSystem explosion2;
+        public ExplosionSmokeParticleSystem smoke;
+
+        SpriteFont font;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            this.graphics.PreferredBackBufferWidth = 768*3;
+            this.graphics.PreferredBackBufferWidth = 1024*3;
+            this.graphics.IsFullScreen = true;
+
+            explosion = new ExplosionParticleSystem(this, 1);
+            Components.Add(explosion);
+
+            smoke = new ExplosionSmokeParticleSystem(this, 2);
+            Components.Add(smoke);       
+        }
+
+        public SpriteBatch getSpriteBatch()
+        {
+            return spriteBatch;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            player = new Player();
+            player.Initialize(this, new Vector2(100,100));
+            player.LoadContent("TankPlayer");
+
+            enemies = new List<Enemy>(5);
+           
+            font = Content.Load<SpriteFont>("font");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            //Creates Enemies
+            spawnEnemies(gameTime);
+            
+            //Update Player
+            player.Update(gameTime);
+
+            //Update Enemies
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                //Get current enemy and update
+                Enemy currentEnemy = enemies[i];
+                currentEnemy.Update(gameTime, player);
+            }
+
+            //Detect Collisions
+            detectCollisions();
+         
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            player.Draw(spriteBatch);
+            for(int i = 0; i < enemies.Count; i++)
+                enemies[i].Draw(spriteBatch);
+            drawText();
+            base.Draw(gameTime);
+        }
+
+        public void spawnEnemies(GameTime gameTime)
+        {
+            enemyTimer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (enemyTimer > .9f && enemies.Count < 15)
+            {
+                int random = rand.Next(100);
+                Enemy spawn = new Enemy();
+                if (random % 4 == 0)
+                    spawn.Initialize(this, new Vector2(300, 300));
+                if (random % 4 == 1)
+                    spawn.Initialize(this, new Vector2(this.GraphicsDevice.Viewport.Width - 300, 300));
+                if (random % 4 == 2)
+                    spawn.Initialize(this, new Vector2(100, this.GraphicsDevice.Viewport.Height - 300));
+                if (random % 4 == 3)
+                    spawn.Initialize(this, new Vector2(this.GraphicsDevice.Viewport.Width - 300, this.GraphicsDevice.Viewport.Height - 300));
+
+                spawn.LoadContent("Tank");
+                enemies.Add(spawn);
+                enemyTimer = 0;
+            }
+        }
+
+        public void detectCollisions()
+        {
+            //COLLISION DETECTION
+            Rectangle playerRect = new Rectangle((int)player.getPosition().X, (int)player.getPosition().Y, player.getTexture().Width, player.getTexture().Height);
+
+            //Loop through all enemies
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                //Get current enemy and creat collision box
+                Enemy currentEnemy = enemies[i];  
+                Rectangle currentEnemyRect = new Rectangle((int)currentEnemy.getPosition().X, (int)currentEnemy.getPosition().Y, currentEnemy.getTexture().Width, currentEnemy.getTexture().Height);
+
+                //Check if player is hit by any of current enemy's projectiles
+                for (int j = 0; j < currentEnemy.getProjectiles().Count; j++)
+                {
+                    Projectile proj = currentEnemy.getProjectiles()[j];
+                    Rectangle enemyProjectileRect = new Rectangle((int)proj.getPosition().X, (int)proj.getPosition().Y, proj.getTexture().Width, proj.getTexture().Height);
+                    if (playerRect.Intersects(enemyProjectileRect))
+                    {
+                        currentEnemy.getProjectiles().Remove(proj);
+                        player.Hit();
+                    }
+                }
+
+                //Check if current enemy is hit by any of player's projectiles
+                for (int j = 0; j < player.getProjectiles().Count; j++)
+                {
+                    Projectile proj = player.getProjectiles()[j];
+                    Rectangle playerProjectileRect = new Rectangle((int)proj.getPosition().X, (int)proj.getPosition().Y, proj.getTexture().Width, proj.getTexture().Height);
+                    if (currentEnemyRect.Intersects(playerProjectileRect))
+                    {
+                        player.getProjectiles().Remove(proj);
+                        currentEnemy.Hit();
+                        if (enemies[i].isDead())
+                        {
+                            enemies.RemoveAt(i);
+                            destroyed++;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Draw Text method for debugging / displaying
+        public void drawText()
+        {
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, "Enemies Destroyed: " + destroyed, new Vector2(30, 45), Color.White);
+            spriteBatch.DrawString(font, "Player Deaths: " + player.getTimesHit(), new Vector2(30, 75), Color.White);
+            spriteBatch.End();
+        }
+    }
+}
---
+++ trunk/Towers/RTS/RTS/Player.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace RTS
+{
+    class Player
+    {
+        Game1 game;
+
+        ContentManager contentManager;
+        GraphicsDevice graphicsDevice;
+        KeyboardState keystate;
+        MouseState oldMousestate;
+        MouseState mousestate;
+        SpriteBatch spriteBatch;
+
+        GamePadState currentState;
+        GamePadState oldState;
+
+        private Texture2D texture;
+        private Texture2D turretTexture;
+        private Texture2D mouseTexture;
+
+        private Vector2 position;
+        private Vector2 mousePos;
+        private Vector2 origin;
+
+        private List<Projectile> projectileList = new List<Projectile>(5);
+
+        private float xComponent = 0;
+        private float yComponent = 0;
+
+        private double speed = 0;
+        private int timesHit = 0;
+
+        private double moveRotationAngle;
+        private double shootRotationAngle;
+
+        private float elapsedTime;
+        private float circle = MathHelper.Pi * 2;
+
+        public float getTurretLength()
+        {
+            return turretTexture.Width;
+        }
+
+        public void Initialize(Game1 game, Vector2 startPosition)
+        {
+            this.game = game;
+            contentManager = game.Content;
+            graphicsDevice = game.GraphicsDevice;
+            position = startPosition;
+            currentState = GamePad.GetState(PlayerIndex.One);
+        }
+
+        public void LoadContent(String textureName)
+        {
+            texture = contentManager.Load<Texture2D>(textureName);
+            turretTexture = contentManager.Load<Texture2D>("TurretPlayer");
+            mouseTexture = contentManager.Load<Texture2D>("Icon");
+            origin.X = texture.Width / 2;
+            origin.Y = texture.Height / 2;
+        }
+
+        public void Draw(SpriteBatch SB)
+        {
+            spriteBatch = SB;
+            spriteBatch.Begin();
+            if (!currentState.IsConnected)
+            {
+                spriteBatch.Draw(mouseTexture, mousePos, null, Color.White, 0f, Vector2.Zero, 1.0f, SpriteEffects.None, 0f);
+            }
+            spriteBatch.Draw(texture, position,null, Color.White, (float)moveRotationAngle, origin, 1.0f, SpriteEffects.None, 0f);
+            spriteBatch.Draw(turretTexture, position, null, Color.White, (float)shootRotationAngle, new Vector2(0,turretTexture.Height/2), 1.0f, SpriteEffects.None, 0f);
+            foreach (Projectile proj in projectileList)
+            {
+                proj.Draw();
+            }
+
+            spriteBatch.End();
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            //game.DrawText();
+
+            elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+           
+            // Get the game pad state.
+            currentState = GamePad.GetState(PlayerIndex.One);
+            
+            if (currentState.IsConnected)
+            {
+                if (Math.Abs(currentState.ThumbSticks.Left.X) > 0 || Math.Abs(currentState.ThumbSticks.Left.Y) > 0)
+                {
+                    moveRotationAngle = Math.Atan2(-currentState.ThumbSticks.Left.Y, currentState.ThumbSticks.Left.X);
+                    moveRotationAngle = moveRotationAngle % circle;
+                }
+
+                if (currentState.ThumbSticks.Left.X != 0 || currentState.ThumbSticks.Left.Y != 0)
+                    speed = 5;
+                else
+                {
+                    //Deprecate speed
+                    speed *= .85f;
+                    speed *= .85f;
+                }
+
+                //Shoot angle between player position and mouse position
+                if (Math.Abs(currentState.ThumbSticks.Right.X) > 0 || Math.Abs(currentState.ThumbSticks.Right.Y) > 0)
+                {
+                    shootRotationAngle = Math.Atan2(-currentState.ThumbSticks.Right.Y, currentState.ThumbSticks.Right.X);
+                    shootRotationAngle = shootRotationAngle % circle;
+                }
+
+                if (currentState.Triggers.Right >= .5f && oldState.Triggers.Right < .5)
+                {
+                    Projectile projectile = new Projectile();
+                    projectile.Initialize(contentManager, graphicsDevice, position, (float)shootRotationAngle, getTurretLength(), 30f);
+                    projectile.LoadContent("Projectile");
+                    projectileList.Add(projectile);
+
+                    game.explosion.AddParticles(new Vector2(position.X + (float)Math.Cos(shootRotationAngle) * getTurretLength(), position.Y + (float)Math.Sin(shootRotationAngle) * getTurretLength()));
+                    game.smoke.AddParticles(new Vector2(position.X + (float)Math.Cos(shootRotationAngle) * getTurretLength(), position.Y + (float)Math.Sin(shootRotationAngle) * getTurretLength())); 
+                }
+            }
+            else
+            {
+                keystate = Keyboard.GetState();
+                mousestate = Mouse.GetState();
+                mousePos.X = mousestate.X;
+                mousePos.Y = mousestate.Y;
+
+                speed = 0;
+                if (keystate.IsKeyDown(Keys.A))
+                { 
+                    //moveRotationAngle -= elapsedTime * 3;
+                   // moveRotationAngle = moveRotationAngle % circle;   
+                    xComponent = -1;
+                    speed = 5;
+   
+                }
+                if (keystate.IsKeyDown(Keys.D))
+                {
+                   // moveRotationAngle += elapsedTime * 3;
+                    //moveRotationAngle = moveRotationAngle % circle;  
+                    xComponent = 1;
+                    speed = 5;
+                }
+                if (keystate.IsKeyDown(Keys.W))
+                {
+                    //speed = 6;
+                    yComponent = -1;
+                    speed = 5;
+                }
+                else if (keystate.IsKeyDown(Keys.S))
+                {
+                    //speed = -1;
+                    yComponent = 1;
+                    speed = 5;
+                }
+                else
+                {
+                    //Deprecate speed
+                    speed *= .85f;
+                    speed *= .85f;
+                }
+                if (keystate.IsKeyDown(Keys.Enter))
+                {
+                    position = new Vector2(100, 100);
+                }
+
+                //Update moveRotationAngle
+                moveRotationAngle = Math.Atan2(yComponent, xComponent);
+                moveRotationAngle = moveRotationAngle % circle;
+                //Reset Components
+                xComponent = 0;
+                yComponent = 0;
+
+                //Shoot angle between player position and mouse position
+                shootRotationAngle = Math.Atan2(mousePos.Y - position.Y, mousePos.X - position.X);
+
+                //If mouse is clicked and released
+                if (oldMousestate.LeftButton == ButtonState.Pressed && mousestate.LeftButton == ButtonState.Released)
+                {
+                    Projectile projectile = new Projectile();
+                    projectile.Initialize(contentManager, graphicsDevice, position, (float)shootRotationAngle, getTurretLength(), 30f);
+                    projectile.LoadContent("Projectile");
+                    projectileList.Add(projectile);
+
+                    game.explosion.AddParticles(new Vector2(position.X + (float)Math.Cos(shootRotationAngle) * getTurretLength(), position.Y + (float)Math.Sin(shootRotationAngle) * getTurretLength()));
+                    game.smoke.AddParticles(new Vector2(position.X + (float)Math.Cos(shootRotationAngle) * getTurretLength(), position.Y + (float)Math.Sin(shootRotationAngle) * getTurretLength()));
+                }
+
+               
+            }
+
+            //Update position based on speed and angle
+            position.X += (float)(Math.Cos(moveRotationAngle) * speed);
+            position.Y += (float)(Math.Sin(moveRotationAngle) * speed);
+            if (position.X > graphicsDevice.Viewport.Width)
+                position.X = graphicsDevice.Viewport.Width;
+            else if (position.X < 0)
+                position.X = 0;
+            if (position.Y > graphicsDevice.Viewport.Height)
+                position.Y = graphicsDevice.Viewport.Height;
+            else if (position.Y < 0)
+                position.Y = 0;
+
+            for(int i = 0; i < projectileList.Count; i++)
+            {
+                //Remove Projectile if it goes off-screen
+                Projectile proj = projectileList[i];
+                if (proj.getPosition().X > graphicsDevice.Viewport.Width || proj.getPosition().X < 0
+                    || proj.getPosition().Y > graphicsDevice.Viewport.Height || proj.getPosition().Y < 0)
+                {
+                    projectileList.Remove(proj);
+                }
+                proj.Update(gameTime);
+            }
+
+            oldMousestate = mousestate;
+            oldState = currentState;
+        }
+
+        public Texture2D getTexture()
+        {
+            return texture;
+        }
+
+        public void setPosition(Vector2 pos)
+        {
+            position = pos;
+        }
+
+        public Vector2 getPosition()
+        {
+            return position;
+        }
+
+        public List<Projectile> getProjectiles()
+        {
+            return projectileList;
+        }
+
+        public float getProjectileCount()
+        {
+            return projectileList.Count;
+        }
+
+        public double getShootRotationAngle()
+        {
+            return shootRotationAngle;
+        }
+
+        public double getMoveRotationAngle()
+        {
+            return moveRotationAngle;
+        }
+
+        public int getTimesHit()
+        {
+            return timesHit;
+        }
+
+        //If the player was hit by an enemy or projectile
+        public void Hit()
+        {
+            timesHit++;
+            if (timesHit % 4 == 0)
+                setPosition(new Vector2(100, 100));
+            if (timesHit % 4 == 1)
+                setPosition(new Vector2(graphicsDevice.Viewport.Width - 100, 100));
+            if (timesHit % 4 == 2)
+                setPosition(new Vector2(100, graphicsDevice.Viewport.Height - 100));
+            if (timesHit % 4 == 3)
+                setPosition(new Vector2(graphicsDevice.Viewport.Width - 100, graphicsDevice.Viewport.Height - 100));
+        }
+
+    }
+}
