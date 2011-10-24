1477392849ab4de8aeef59d4bbe8b603de0e7ad5
csoon03@gmail.com <csoon03@gmail.com@282f65b9-ccb7-5106-6a69-96a0916cf8f3>
2011-10-24 06:47:16 +0000

    Added base class Sprite; Player, Enemy and Tower inherits from Sprite class.

---
+++ Towers/RTS/RTS/EnemyEffect.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RTS
+{
+    abstract class EnemyEffect
+    {
+        protected int created = 0;
+        protected int duration;
+        protected int life;
+
+        protected Texture2D effect;
+        public Texture2D Effect
+        {
+            get { return effect; }
+        }
+
+        public abstract void applyEffects(Enemy enemy);
+        public abstract void undoEffect(Enemy enemy);
+
+        public Boolean isValid(GameTime time)
+        {
+            life += time.ElapsedGameTime.Milliseconds;
+            if (life < duration * 1000)
+                return true;
+            else return false;
+        }
+
+    }
+
+    class EnemyEffectBurn : EnemyEffect
+    {
+        protected int burnDmg;
+
+        public EnemyEffectBurn(Game1 game, int duration, int burnDmg)
+        {
+            this.duration = duration;
+            this.burnDmg = burnDmg;
+            effect = game.Content.Load<Texture2D>("burn");
+        }
+
+        public override void applyEffects(Enemy enemy)
+        {
+            enemy.effectDamage(burnDmg);
+        }
+        public override void undoEffect(Enemy enemy)
+        {
+
+        }
+
+
+
+    }
+
+
+
+}
+
+
