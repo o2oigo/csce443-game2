1c7b351c20a0ba3a429a185902bc9a3210bbd98d
csoon03@gmail.com <csoon03@gmail.com@282f65b9-ccb7-5106-6a69-96a0916cf8f3>
2011-10-31 07:02:21 +0000

    Different enemies (High HP, Faster Movement, more to be added later)

---
+++ trunk/Towers/RTS/RTS/Enemies.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace RTS
+{
+    class HPEnemy : Enemy
+    {
+        public override void Initialize(Game1 game, Vector2 startPosition, Map map)
+        {
+            base.Initialize(game, startPosition, map);
+
+            range = 200;
+            hp = 300;
+            weakAgainst = ElementType.Fire;
+            strongAgainst = ElementType.Water;
+
+        }
+
+        public virtual void LoadContent()
+        {
+            base.LoadContent("TankEnemy");
+        }
+    }
+
+    class FastEnemy : Enemy
+    {
+        public override void Initialize(Game1 game, Vector2 startPosition, Map map)
+        {
+            base.Initialize(game, startPosition, map);
+
+            range = 200;
+            hp = 300;
+            weakAgainst = ElementType.Water;
+            strongAgainst = ElementType.Fire;
+            moveSpeed = 200.0f;
+        }
+
+        public virtual void LoadContent()
+        {
+            base.LoadContent("TankPurple");
+        }
+    }
+
+
+}
